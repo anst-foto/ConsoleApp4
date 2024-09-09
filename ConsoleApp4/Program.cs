b44e1325b2005963de321c0674571fc708857fd6
Starinin Andrey <starininandrey@gmail.com>
2024-09-09 14:17:05 +0300

    Update

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿const int N = 3;
+const int M = 3;
+var array = new int [M, N];
+
+for (int i = 0; i < M; i++)
+{
+    for (int j = 0; j < N; j++)
+    {
+        array[i, j] = i * N + j;
+    }
+}
+
+var max = array.Cast<int>().Max();
+var min = array.Cast<int>().Min();
+
+var avgs = new double[M];
+for (int i = 0; i < M; i++)
+{
+    var sum = 0;
+    for (int j = 0; j < N; j++)
+    {
+        sum += array[i, j];
+    }
+    
+    avgs[i] = (double)sum / N;
+}
+
+for (int i = 0; i < M; i++)
+{
+    for (int j = 0; j < N; j++)
+    {
+        Console.Write(array[i, j] + " ");
+    }
+
+    Console.WriteLine();
+}
+Console.WriteLine($"Max: {max}, Min: {min}");
+for(int i = 0; i < M; i++)
+{
+    Console.WriteLine($"avg [{i}]: {avgs[i]}");
+}
